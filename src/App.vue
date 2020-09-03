806aaa69a4fc6d6bfb0ef59b401aad42788c2920
TARISO <toni.ariso@gmail.com>
2020-09-03 09:53:49 +0200

    jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -20,23 +21,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true,
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
@@ -44,8 +29,23 @@ export default {
       this.todos = this.todos.filter((todo) => todo.id != id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios
+        .post('https://jsonplaceholder.typicode.com/todos', {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+
+        .catch((err) => console.log(err));
     },
+  },
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
