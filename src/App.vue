bb39dd01070131b7a1ed4d46bcede6413c528dac
TARISO <toni.ariso@gmail.com>
2020-09-03 10:20:11 +0200

    delete

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,13 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id != id);
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(
+          // eslint-disable-next-line
+          (res) => (this.todos = this.todos.filter((todo) => todo.id != id))
+        )
+        .catch((err) => console.log(err));
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
